5fd3ec13dd8f87bbd7f61737ce5387a39e740a8d
Daniel <danielyapura@gmail.com>
2020-04-08 15:34:49 -0300

    modificaciones y agregar el botón actualizar

--- app/src/main/java/com/danielyapura/covid19/ServicesDownloadData.kt
+++ app/src/main/java/com/danielyapura/covid19/ServicesDownloadData.kt
@@ -7,9 +7,6 @@ import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.JobIntentService
-import com.danielyapura.covid19.R
-import com.danielyapura.covid19.RetrofitServiceGenerator
-import com.danielyapura.covid19.WidgetProvider
 import retrofit2.Call
 import retrofit2.Response
 import java.text.SimpleDateFormat
@@ -19,27 +16,25 @@ import java.util.*
 class ServicesDownloadData: JobIntentService() {
 
     companion object {
+        private const val JOB_ID: Int = 10
 
-        val JOB_ID: Int = 1
-
-        public fun enqueueWork(context: Context, intent: Intent) {
+        fun enqueueWork(context: Context, intent: Intent) {
             enqueueWork(context, ServicesDownloadData::class.java, JOB_ID, intent)
         }
     }
 
-
     override fun onHandleWork(intent: Intent) {
         Log.d("SERVICIO", "Servicio iniciado")
 
-        val views: RemoteViews = RemoteViews(packageName, R.layout.coronavirus_widget_layout)
-        val theWidget: ComponentName = ComponentName(this, WidgetProvider::class.java)
+        val views = RemoteViews(packageName, R.layout.coronavirus_widget_layout)
+        val theWidget = ComponentName(this, WidgetProvider::class.java)
         val widgetManager: AppWidgetManager = AppWidgetManager.getInstance(this)
 
         //mostrar fecha de actualización
         val currentDateTime = Calendar.getInstance()
         val format1 = SimpleDateFormat("dd/MM/yyyy - HH:mm")
         val formatted = format1.format(currentDateTime.time)
-        views.setTextViewText(R.id.ultimo_registro, formatted)
+        views.setTextViewText(R.id.fecha_txt, formatted)
 
         val covidAPI: CovidAPI = RetrofitServiceGenerator.createService(CovidAPI::class.java)
 
@@ -51,7 +46,7 @@ class ServicesDownloadData: JobIntentService() {
             val dataConfirmados: Data? = response.body()?.last()
             views.setTextViewText(R.id.confirmadosTv, dataConfirmados?.cases.toString())
             widgetManager.updateAppWidget(theWidget, views)
-            Log.d("DATOS-OK", "CONFIRMADOS")
+
         } catch (ex: Exception){
             Log.d("DATOS-ERROR", "CONFIRMADOS")
             ex.printStackTrace()
@@ -73,7 +68,6 @@ class ServicesDownloadData: JobIntentService() {
         }
 
         widgetManager.updateAppWidget(theWidget, views)
-        //this.stopSelf()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/danielyapura/covid19/WidgetProvider.kt
+++ app/src/main/java/com/danielyapura/covid19/WidgetProvider.kt
@@ -8,26 +8,37 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
-import android.widget.Toast
-import com.danielyapura.covid19.ServicesDownloadData
-
 
 /**
  * Implementation of App Widget functionality.
  */
+
+private const val TAG_LOG = "TAG-CORONAVIRUS"
+
 class WidgetProvider : AppWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
+        Log.d(TAG_LOG, "OnUPDATE")
 
-        Toast.makeText(context, "Actualizando", Toast.LENGTH_SHORT).show()
+        actualizarIntefaz(context, appWidgetManager, appWidgetIds)
 
-        Log.d("TAG-CORONAVIRUS", "OnUpdate")
+        configurarBotonActualizar(context, appWidgetManager, appWidgetIds)
+
+        val intent = Intent(context, ServicesDownloadData::class.java)
+        ServicesDownloadData.enqueueWork(context, intent)
+    }
+
+    private fun configurarBotonActualizar(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
 
         val intentBoton = Intent(context, WidgetProvider::class.java)
-        intentBoton.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+        intentBoton.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
         val pendingIntent =
             PendingIntent.getBroadcast(context, 0, intentBoton, PendingIntent.FLAG_UPDATE_CURRENT)
 
@@ -37,42 +48,50 @@ class WidgetProvider : AppWidgetProvider() {
         ).apply {
             setOnClickPendingIntent(R.id.actualizar_bt, pendingIntent)
         }
+
         appWidgetManager.updateAppWidget(appWidgetIds, views)
+    }
 
-        val intent = Intent(context, ServicesDownloadData::class.java)
-        ServicesDownloadData.enqueueWork(context, intent)
+    private fun actualizarIntefaz(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        val views = RemoteViews(context.packageName, R.layout.coronavirus_widget_layout)
+        views.setTextViewText(R.id.confirmadosTv, "--")
+        views.setTextViewText(R.id.muertesTv, "--")
+        views.setTextViewText(R.id.fecha_txt, "Actualizando...")
 
+        appWidgetManager.updateAppWidget(appWidgetIds, views)
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val appWidgetManager = AppWidgetManager.getInstance(context)
         val thisAppWidget = ComponentName(
-            context!!.packageName,
-            WidgetProvider::class.java.getName()
+            context!!,
+            WidgetProvider::class.java
         )
+
         val appWidgetIds = appWidgetManager.getAppWidgetIds(thisAppWidget)
-        Log.d("TAG-CORONAVIRUS", "ONRECEIVE")
+        Log.d(TAG_LOG, "ONRECEIVE")
         onUpdate(context, appWidgetManager, appWidgetIds)
     }
 
     override fun onEnabled(context: Context?) {
-        Log.d("TAG-CORONAVIRUS", "ENABLED")
-        //super.onEnabled(context)
+        Log.d(TAG_LOG, "OnENABLED")
     }
 
     override fun onRestored(context: Context?, oldWidgetIds: IntArray?, newWidgetIds: IntArray?) {
-        Log.d("TAG-CORONAVIRUS", "RESTORED")
-        //super.onRestored(context, oldWidgetIds, newWidgetIds)
+        Log.d(TAG_LOG, "OnRESTORED")
     }
 
     override fun onDisabled(context: Context?) {
-        Log.d("TAG-CORONAVIRUS", "DISABLED")
-        //super.onDisabled(context)
+        Log.d(TAG_LOG, "OnDISABLED")
     }
 
     override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
-        Log.d("TAG-CORONAVIRUS", "DELETE")
-        //super.onDeleted(context, appWidgetIds)
+        Log.d(TAG_LOG, "OnDELETE")
     }
+
 }
 
