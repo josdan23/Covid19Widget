4d4b7401aa6e0af06d8338f52e0f266cefe813c7
Daniel <danielyapura@gmail.com>
2020-04-14 19:45:32 -0300

    Agregar JobServices para optimizar las tareas background en android

---
+++ app/src/main/java/com/danielyapura/covid19/DownloadDataJob.kt
@@ -0,0 +1,32 @@
+package com.danielyapura.covid19
+
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+
+
+class DownloadDataJob: JobService() {
+    override fun onStopJob(p0: JobParameters?): Boolean {
+
+        Log.d("JOB-SERVICE", "TERMINADO")
+        return false //es true si se sigue ejecutando, llamar a jobFinished
+    }
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        Log.d("JOB-SERVICE", "INICIADO")
+
+        ejecutarTarea()
+        return true
+    }
+
+    private fun ejecutarTarea() {
+        Log.d("JOB-SERVICES", "EJECUTAR TAREA")
+        Toast.makeText(applicationContext, "Actualizando", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(applicationContext, DownloadDataIntentService::class.java)
+        startService(intent)
+    }
+
+}
